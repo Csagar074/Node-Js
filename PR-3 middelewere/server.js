5533ab10f3d618bbb654667283f9a13726217c0b
Sagar0721 <Sagar963963@gmail.com>
2025-12-11 15:13:51 +0530

    PR-3 Middelewere

--- PR-3 middelewere/server.js
+++ PR-3 middelewere/server.js
@@ -4,14 +4,14 @@ const app = express();
 const port = 8001;
 
 app.set('view engine', 'ejs')
-app.use('/', express.static('public'));
+app.use(express.static(__dirname + "/Public"));
 
 
 app.get('/', (req, res) => {
-    res.render('ageCheck');
+    res.render("ageCheck");
 });
 
-const middleware = (req, res, next) => {
+const middle = (req, res, next) => {
 
     if (req.query.age >= 18) {
         next();
@@ -19,13 +19,15 @@ const middleware = (req, res, next) => {
         return res.redirect('/404');
     }
 }
-app.get('/404', (req, res) => {
-    res.render('404');
+
+app.get('/home', middle, (req, res) => {
+    res.render("home");
 })
 
-app.get('/home', middleware, (req, res) => {
-    res.render('home');
+app.get('/404', (req, res) => {
+    res.render("404");
 })
+
 
 app.listen(port, (err) => {
     if (err) {

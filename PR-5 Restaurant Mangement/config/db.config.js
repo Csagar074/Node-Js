c3560580af8197038b4c54f5c79a2cc7be87ab5b
Sagar0721 <Sagar963963@gmail.com>
2026-01-01 22:50:46 +0530

    MVC Structure CURD

--- PR-5 Restaurant Mangement/config/db.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-mongoose.connect(URI).then(()=>{
-    console.log("Mongodb is Connected....");
-    
-}).catch((err)=>{
-    console.log("Mongodb is Failed....",err);
-    
-})

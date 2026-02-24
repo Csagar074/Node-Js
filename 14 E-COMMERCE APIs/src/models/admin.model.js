774bdce112fddfa85a5e083641a932e48c0aee53
Sagar0721 <Sagar963963@gmail.com>
2026-02-24 23:24:37 +0530

    middleware

--- 14 E-COMMERCE APIs/src/models/admin.model.js
+++ 14 E-COMMERCE APIs/src/models/admin.model.js
@@ -1,46 +1,46 @@
 
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const adminschema=mongoose.Schema({
-    first_name:{
-        type:String,
-        required:true
+const adminschema = mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
     },
-       last_name:{
-        type:String,
-        required:true
+    last_name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    
-    password:{
-        type:String,
-        required:true
+
+    password: {
+        type: String,
+        required: true
     },
-    phone:{
-        type:Number,
-        required:true
+    phone: {
+        type: Number,
+        required: true
     },
-    isActive:{
-        
-        type:Boolean,
-        default:true
+    isActive: {
+
+        type: Boolean,
+        default: true
     },
-    isDelete:{
-        type:Boolean,
-        default:false
-    },create_at:{
-        type:String,
-        required:true
+    isDelete: {
+        type: Boolean,
+        default: false
+    }, create_at: {
+        type: String,
+        required: true
     },
-    update_at:{
-        
-        type:String,
-        required:true
+    update_at: {
+
+        type: String,
+        required: true
 
     }
 });
-module.exports=mongoose.model('Admin',adminschema,'Admin');
+module.exports = mongoose.model('Admin', adminschema, 'Admin');

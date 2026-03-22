cf616193cce26eb8e3d1971f1a171968ce1ab9c5
Sagar0721 <Sagar963963@gmail.com>
2026-03-22 10:53:53 +0530

    E-commerce Apis

---
+++ 14 E-COMMERCE APIs/src/utils/response.js
@@ -0,0 +1,8 @@
+
+module.exports.successResponse = (status, error = false, message, result) => {
+    return { status, error, message, result };
+}
+
+module.exports.errorResponse = (status = 500, error = true, message) => {
+    return { status, error, message };
+}
